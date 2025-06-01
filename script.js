bd63890d4b139d33a54594930a4382a23df4d4d5
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-06-01 17:27:15 -0400

    fixed html and css need to work on the js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-const inputText = document.getElementById("inputText");
-const createData = document.getElementById("createData");
-const listItems = document.getElementById("listItems");
-const saveData = document.getElementById("saveData");
-const deleteData = document.getElementById("deleteData");
+let inputText = document.getElementById("inputText");
+let createData = document.getElementById("createData");
+let saveData = document.getElementById("saveData");
 
 function create() {
-    if(inputText == "") {
+    const inputText = input.value.text();
+
+    if(inputItems === "") {
         window.alert("Please enter a task");
+    } else {
+        let listItem = document.createElement("li");
+        listItem.textContent = inputItems;
+        saveData.appendChild(listItem);
+        inputText.value = "";
+        }
+        createData.onclick = create;
     }
-    else if(inputText != "") {
-        let listItems = document.createElement("li");
-        listItems.innerHTML = inputText;
-        saveData.appendChild(listItems);
-        let span = `${inputText}`
-        span.innerHTML = `${inputText}`;
-    }
-}
