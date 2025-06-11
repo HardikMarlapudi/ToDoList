2ccf05d4c4fbb2cdc925d30c84a4769fa78b295d
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-06-10 22:31:32 -0400

    fix the footer and font awesome buttons alignment

--- script.js
+++ script.js
@@ -1,37 +1,30 @@
 let inputText = document.getElementById("inputText");
 let createData = document.getElementById("createData");
-let removeData = document.getElementById("removeData");
-let saveData = document.getElementById("saveData");
+let editData = document.getElementById("fa-pen-to-square");
+let removeData = document.getElementById("fa-trash");
 
 function create() {
-    const inputItems = inputText.value.trim();
+    let newElement = document.createElement("li");
+    let text = document.createTextNode(inputText.value);
+    newElement.appendChild(text);
+    saveData.appendChild(newElement);
+    inputText.value = "";
+}
 
-    if(inputItems === "") {
-        window.alert("Please enter a task, you would like to add");
+function edit() {
+    if (document.getElementById("createData").textContent === "") {
+        editData.disabled = true;
     } else {
-        let listItem = document.createElement("li");
-        listItem.textContent = inputItems;
-        saveData.appendChild(listItem);
-        inputText.value = "";
-        }
-        createData.onclick = create;
-    try {
-        inputText.value === inputItems;
-    } catch (error) {
-        window.alert("You have already completed this task");
+        let editElement = saveData.lastElementChild;
+        let text = prompt("Enter new text");
+        editElement.textContent = text;
     }
 }
 
 function remove() {
-    const inputItems = inputText.value.trim();
-
-    if (inputItems === "") {
-        window.alert("Please enter a task, you would like to remove");
+    if (document.getElementById("createData").textContent === "") {
+        removeData.disabled = true;
     } else {
-        let listItem = document.createElement("li");
-        listItem.textContent = inputItems;
-        saveData.remove(listItem);
-        inputText.value = "";
+        saveData.removeChild(saveData.lastElementChild);
     }
-    removeData.onclick = remove;
 }
