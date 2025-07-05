7f02f2efb6755aafbfa47f11af81df5969b33c48
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-07-05 15:28:43 -0400

    reorganized html and Javascript functionality

--- ToDoList/src/ToDoList.jsx
+++ ToDoList/src/ToDoList.jsx
@@ -3,58 +3,55 @@ import './ToDoList.css';
 
 function ToDoList() {
 
-    const [tasks, setTasks] = useState(["Walk the Dog", "Complete HomeWork", "Do the Chores"]);
+    const [tasks, setTasks] = useState(["Walk the Dog", "Complete Homework", "Do the Chores"]);
     const [newTask, setNewTask] = useState("");
 
-    function setTask(index) {
-        event.target.value()
+    function handleInputChange(event) {
+        setNewTask(event.target.value);
     }
 
     function addTask(index) {
-        if(index < 0) {
+       if (index < 0) {
         setTasks(t => [...t, newTask]);
         setNewTask("");
         }
     }
 
     function removeTask(index) {
-       if(index) {
-        setTasks(tasks.filter((_, i) => i !== index));
-       }
+        if(index < tasks.length) {
+            setTasks(tasks.filter((_, i) => i !== index));
+        }
+    }
+
+    function moveTaskUp(index) {
+        if(index > 0) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
+    }
+
+    function moveTaskDown(index) {
+        if(index < tasks.length - 1) {
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
     return(
         <>
         <h1>To-Do-List</h1>
         <div className='to-do-list'>
-            <input type="text" placeholder='Enter Task...' id="taskInput" value={newTask} onChange={(e) => {
-                setNewTask(e.target.value);
-            }}/>
-            <button className='add-button' onClick={() => {
-                setTasks(t => [...t, newTask]);
-                setNewTask("");
-            }}>Add</button>
+            <input type="text" placeholder='Enter Task...' id="taskInput" value={newTask} onChange={handleInputChange}/>
+            <button className='add-button' onClick={() => addTask (-1)}>Add</button>
             <div className='list'>
                 {tasks.map((task, index) => {
                     return <div className='task' key={index}>
                         <span className='task-name'>{task}</span>
-                        <button className='delete-button' onClick={() => {
-                            setTasks(tasks.filter((_, i) => i !== index));
-                        }}>Delete</button>
-                        <button className="upArrow" onClick={() => {
-                            if(index > 0) {
-                                const updatedTasks = [...tasks];
-                                [updatedTasks[index], updatedTasks[index - 1]] = [updatedTasks[index - 1], updatedTasks[index]];
-                                setTasks(updatedTasks);
-                            }
-                        }}>Up</button>
-                        <button className="downArrow" onClick={() => {
-                            if(index < tasks.length - 1) {
-                                const updatedTasks = [...tasks];
-                                [updatedTasks[index], updatedTasks[index + 1]] = [updatedTasks[index + 1], updatedTasks[index]];
-                                setTasks(updatedTasks);
-                            }
-                        }}>Down</button>
+                        <button className='delete-button' onClick={() => removeTask (index)}>Delete</button>
+                        <button className="upArrow" onClick={() => moveTaskUp (index)}>Up</button>
+                        <button className="downArrow" onClick={() => moveTaskDown (index)}>Down</button>
                     </div>
                 })}
             </div>
