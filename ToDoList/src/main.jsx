471b7b19b689e0c0fe1783bf3e331dbd506505ec
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-11-12 23:18:16 -0500

    removed all unncessary files from the program

--- ToDoList/src/main.jsx
+++ ToDoList/src/main.jsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import './Footer.css';
-import Footer from './Footer.jsx';
 import ToDoList from './ToDoList.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ToDoList />
-    <Footer />
   </StrictMode>,
 )
